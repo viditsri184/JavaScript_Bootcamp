aa353c28c120ece10f9805c3518c979e916516ed
Vidit Srivastava <148772849+viditsri184@users.noreply.github.com>
2024-04-23 16:30:42 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// code readability should be high
+console.log(3+3)
+console.log("Vidit")
+
+let name = "Vidit"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
