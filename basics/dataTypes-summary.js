72ea3e8c2973a278809c15f3a83cf6594770e1f7
Vidit Srivastava <148772849+viditsri184@users.noreply.github.com>
2024-04-30 15:23:32 +0000

    datatype-summary

---
+++ basics/dataTypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive :
+/*
+These are call by value
+7 Types : String, Boolean, Number, null, undefined, Symbol, BigInt
+ */
+
+// Reference / Non-Primitive type: 
+
+// Dynamically typed language
+
+const id = Symbol('1241');
+const anotherId = Symbol('1231');
+
+console.log(id === anotherId);
+
+// const bigNumber = 14414124141421
+
+/*
+Array, Objects, Functions
+ */
+
+const fruits = ["apple", "mango" , "guava"];
+
+let myObj = {
+    name: "Vidit",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
